0fd9450005b522b5751058776fcb1b9708f4e855
Imran Khan <imrankhan@teknik.io>
2020-09-08 18:29:23 +0600

    Added release bumping for github packages [CI skip]
    We don't want to be implementing half of xtools poorly, so should stop.

--- github_upgrade.sh
+++ github_upgrade.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
-package="$1"
+package="$2"
 template=srcpkgs/$package/template
 
 repo=$(perl -ne 'print $1 if /github.com\/(.*?)\/archive/' < $template)
 
-old_revision=$(grep -Po 'revision=\K.*' $template)
-new_revision=$(expr $old_revision + 1)
 
-last_commit=$(curl -sL https://api.github.com/repos/$repo/commits/master | jq -r .sha)
-echo $last_commit
+release() {
+  new_tag=$(curl -sL https://api.github.com/repos/$repo/releases/latest | jq -r .tag_name)
+  new_version=$(echo $new_tag | perl -pe 's/\D+(.*\d).*/$1/')
+  wget -O /tmp/$package.tar.gz https://github.com/$repo/archive/$new_tag.tar.gz
+  sed -i -E "\|^_version|s|=.*|=$new_version|" $template
+}
 
-wget -O /tmp/$package.tar.gz https://github.com/$repo/archive/$last_commit.tar.gz
+master() {
+  last_commit=$(curl -sL https://api.github.com/repos/$repo/commits/master | jq -r .sha)
+  old_revision=$(grep -Po 'revision=\K.*' $template)
+  new_revision=$(expr $old_revision + 1)
+  wget -O /tmp/$package.tar.gz https://github.com/$repo/archive/$last_commit.tar.gz
+  sed -i -E "\|^_commit|s|=.*|=$last_commit|" $template
+  sed -i -E "\|^revision|s|=.*|=$new_revision|" $template
+}
+
+"$@"
+
 shasum=$(sha256sum /tmp/$package.tar.gz | cut -d' ' -f1)
-
-sed -i -E "\|^_commit|s|=.*|=$last_commit|" $template
-sed -i -E "\|^revision|s|=.*|=$new_revision|" $template
 sed -i -E "\|^checksum|s|=.*|=$shasum|" $template
+rm /tmp/$package*
