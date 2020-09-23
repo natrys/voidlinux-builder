e407720570eb8dbc08906a9248f446c68f00120d
Imran Khan <imrankhan@teknik.io>
2020-09-24 00:50:37 +0600

    fix regex [CI skip]

--- github_upgrade.sh
+++ github_upgrade.sh
@@ -1,41 +1,45 @@
 #!/bin/sh
 
-package="$2"
-template=srcpkgs/$package/template
+pkgname="$2"
+template=srcpkgs/$pkgname/template
 
-repo=$(grep -Po 'github.com/\K(.*?)(?=/archive)' < $template)
+repo=$(grep -Po 'github.com/\K(.*?/.*?)(?=/)' < $template)
 [ -n "$repo" ] || exit 1
+
+export distfile=$(grep -Po "^distfiles=\\K\"https://github.com/$repo/.*\"" < $template)
 
 release() {
   new_tag=$(curl -sL https://api.github.com/repos/$repo/releases/latest | jq -r .tag_name)
   old_version=$(grep -Po '^version=\K.*' $template)
-  new_version=$(echo $new_tag | perl -pe 's/\D+(.*\d).*/$1/')
-  
+  new_version=$(echo $new_tag | perl -pe 's/\D*?(.*\d).*?/$1/')
   [ "$old_version" = "$new_version" ] && exit 1
-  wget -O /tmp/$package.tar.gz https://github.com/$repo/archive/$new_tag.tar.gz
-  sed -i -E "\|^_version|s|=.*|=$new_version|" $template
+
+  tarball=$(pkgname=$pkgname version=$new_version sh -c 'eval echo $distfile')
+  wget -O /tmp/$pkgname.tar.gz $tarball
+  
+  sed -i -E "\|^version|s|=.*|=$new_version|" $template
 }
 
 master() {
-  info=/tmp/$package.commit
+  info=/tmp/$pkgname.commit
   curl -sL https://api.github.com/repos/$repo/commits/master > $info
   old_commit=$(grep -Po '^_commit=\K.*' $template)
   new_commit="$(jq -r .sha $info)"
   [ "$old_commit" = "$new_commit" ] && exit 1
   
-  wget -O /tmp/$package.tar.gz https://github.com/$repo/archive/$new_commit.tar.gz
+  tarball=$(pkgname=$pkgname version=$new_commit sh -c 'eval echo $distfile')
+  wget -O /tmp/$pkgname.tar.gz $tarball
 
-  old_version=$(grep -Po 'version=\K\d{8}' $template)
-  if [ "$old_version" ]; then
-    new_version=$(jq -r .commit.committer.date < $info | sed -E 's|T.*||; s|-||g')
-    if [ "$old_version" = "$new_version" ]; then
+  old_date=$(grep -Po 'version=\K\d{8}' $template)
+  if [ "$old_date" ]; then
+    new_date=$(jq -r .commit.committer.date < $info | sed -E 's|T.*||; s|-||g')
+    if [ "$old_date" = "$new_date" ]; then
       old_revision=$(grep -Po '^revision=\K.*' $template)
       new_revision=$(expr $old_revision + 1)
     else
       new_revision=1
     fi
-    
-    sed -i -E "\|^version|s|=.*|=$new_version|" $template
+    sed -i -E "\|^version|s|=.*|=$new_date|" $template
     rm $info
   fi
 
@@ -45,6 +49,6 @@ master() {
 
 "$@"
 
-shasum=$(sha256sum /tmp/$package.tar.gz | cut -d' ' -f1)
+shasum=$(sha256sum /tmp/$pkgname.tar.gz | cut -d' ' -f1)
 sed -i -E "\|^checksum|s|=.*|=$shasum|" $template
-rm /tmp/$package.tar.gz
+rm /tmp/$pkgname.tar.gz
