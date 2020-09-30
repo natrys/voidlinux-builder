a51b51865abe796c970d4c8d18c0aecdebd352d9
Imran Khan <imrankhan@teknik.io>
2020-09-30 15:23:14 +0600

    fix: emacs-nativecomp

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-/hostrepo/void/xbps-src sort-dependencies $(cat /tmp/unsorted_packages) > /tmp/packages
+cd /hostrepo/
+
+void/xbps-src sort-dependencies $(cat /tmp/unsorted_packages) > /tmp/packages
 
 echo "These packages will be built:"
 cat /tmp/packages
 
 while read package
 do
-  rm -r /hostrepo/void/srcpkgs/$package/
-  cp -r /hostrepo/srcpkgs/$package/ /hostrepo/void/srcpkgs/$package/
-  [ -f /hostrepo/shlibs/$package ] && sh /hostrepo/shlibs/$package /hostrepo/void/common/shlibs
+  rm -r void/srcpkgs/$package/
+  cp -r srcpkgs/$package/ void/srcpkgs/$package/
 done < /tmp/packages ;
+
+cd void/
 
 while read package
 do
-  bin/echo -e "\x1b[32mBuilding package: $package...\x1b[0m"
-  rm -r /hostrepo/void/srcpkgs/$package/
-  cp -r /hostrepo/srcpkgs/$package/ /hostrepo/void/srcpkgs/$package/
-  /hostrepo/void/xbps-src -j$NPROCS $_ARCH pkg "$package"
+  chroot-git restore common/hooks/
+  /bin/echo -e "\x1b[32mBuilding package: $package...\x1b[0m"
+  
+  [ -f srcpkgs/$package/hook ] && sh srcpkgs/$package/hook
+  ./xbps-src -j$NPROCS $_ARCH pkg "$package"
 done < /tmp/packages ;
