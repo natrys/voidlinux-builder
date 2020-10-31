0ac0f47765ef48b53e6772f8b2848cb4a248b0c9
Imran Khan <imrankhan@teknik.io>
2020-11-01 03:48:46 +0600

    update: emacs-nativecomp

--- run.sh
+++ run.sh
@@ -43,7 +43,21 @@ docker run -d \
 
 cd void/
 
-. common/travis/set_mirror.sh
+TRAVIS_PROTO=http
+TRAVIS_MIRROR=mirrors.servercentral.com/voidlinux/
+
+for _i in etc/xbps.d/repos-remote*.conf ; do
+    /bin/echo -e "\x1b[32mUpdating $_i...\x1b[0m"
+    # First fix the proto, ideally we'd serve everything with HTTPS,
+    # but key management and rotation is a pain, and things are signed
+    # so we can afford to be a little lazy at times.
+    sed -i "s:https:$TRAVIS_PROTO:g" $_i
+
+    # Now set the mirror
+    sed -i "s:alpha\.de\.repo\.voidlinux\.org:$TRAVIS_MIRROR:g" $_i
+done
+
+#. common/travis/set_mirror.sh
 . common/travis/prepare.sh
 
 cd ../
